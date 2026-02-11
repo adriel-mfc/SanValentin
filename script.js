ad19f4bc27ccf50613f5d02cf30c6deed00ae197
adriel-mfc <adriel.fox2008@gmail.com>
2026-02-10 23:58:35 -0500

    Restore and update event listeners in script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "YEEEEEEEEEEEEEEE!";
 
     catImg.src = "cat_dance.gif";
 
